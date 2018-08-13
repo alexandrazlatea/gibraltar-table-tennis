d53542c2aabee28c6ed81b65eed7849a49ca45f2
Alexandra Zlatea <alexandra.zlatea@esl-asia.com>
2018-08-13 08:50:53 +0200

    add button

--- src/components/TournamentsList.js
+++ src/components/TournamentsList.js
@@ -2,18 +2,34 @@ import React, {Component} from 'react';
 import {fetchTournaments}  from '../actions/index';
 import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
+import {fire} from "../fire";
 
 class TournamentsList extends Component {
     constructor(props) {
         super(props);
         this.state = {
             tournaments : "",
-            currentUser: null
+            currentUser: null,
+            renderView: "",
+            currentUser: ""
         };
     }
 
     componentDidMount = () => {
         this.props.fetchTournaments();
+        if (localStorage['userId']) {
+            this.checkLoginState();
+        }
+    }
+
+    checkLoginState = () => {
+        fire.auth().onAuthStateChanged(user => {
+            if (!user || (user && user.uid !== localStorage['userId'])) {
+                localStorage.removeItem('userId');
+                this.setState({renderHeader: Math.floor(Math.random() * 90 + 10)})
+                this.props.renderView(Math.floor(Math.random() * 90 + 10));
+            }
+        });
     }
     componentWillReceiveProps(nextProps) {
         this.setState({tournaments: nextProps.tournaments});
@@ -25,11 +41,17 @@ class TournamentsList extends Component {
     }
 
     renderTournaments = () => {
-        const {tournaments} = this.state;
+        const {tournaments, currentUser} = this.state;
         return Object.values(tournaments).map((tournament, index) => {
-            console.log(tournament);
+            console.log(tournament, 'tournament');
             return (
-                <div className="tournament-name">{tournament.name} </div>
+                <div class="tournament">
+                    <li className="tournament-name">{tournament.name} </li>
+                    <div className="startDate"><span>{tournament.start_date} - </span></div>
+                    <div className="endDate"><span> {tournament.end_date}</span></div>
+            { currentUser && <button onClick={this.handleClick} >Join</button> }
+            { !currentUser && <button onClick={this.handleClick} >Login to join</button> }
+                </div>
             )
         });
 
@@ -40,13 +62,14 @@ class TournamentsList extends Component {
         return (
             <div className="tournaments-list">
                 <h1>Tournaments</h1>
-                {this.renderTournaments()}
+                <ul>
+                    {this.renderTournaments()}
+                </ul>
             </div>
         );
     }
 }
 function mapStateToProps(state) {
-    console.log(state, '');
     return {
         tournaments: state.tournaments,
         renderView: state.renderView
--- src/reducers/reducer_fetch_tournaments.js
+++ src/reducers/reducer_fetch_tournaments.js
@@ -1,4 +1,5 @@
 export default function (state = '', action) {
+    console.log(action.payload, 'action payload')
     switch(action.type) {
         case 'FETCH_TOURNAMENTS':
             return action.payload;
